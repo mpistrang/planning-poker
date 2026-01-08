1b792abfb7ed007292e3c91e406ee16bb20dc310
Michael Pistrang <michael.pistrang@livsafe.com>
2026-01-08 07:44:43 -0500

    and frontend

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,17 +1,18 @@
 export const CARD_VALUES = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '5',
-  '8',
-  '13',
-  '21',
-  '?',
-  '☕'
-] as const
+  "0",
+  "0.5",
+  "1",
+  "2",
+  "3",
+  "5",
+  "8",
+  "13",
+  "21",
+  "?",
+  "☕",
+] as const;
 
-export type CardValue = typeof CARD_VALUES[number]
+export type CardValue = (typeof CARD_VALUES)[number];
 
-export const WS_URL = import.meta.env.VITE_WS_URL || 'http://localhost:8000'
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+export const WS_URL = import.meta.env.VITE_WS_URL || "http://localhost:8000";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
